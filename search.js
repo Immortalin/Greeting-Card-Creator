1cd724e021ca9af0b79a189ef3766980897dcb7a
Immortalin <immortailn@icloud.com>
2017-06-26 21:50:37 -0700

    updates!

--- search.js
+++ search.js
@@ -1,3 +1,22 @@
+$('#photoSearch').on('keypress', (e) => {
+    if (e.which == 13) {
+        searchPicture();
+    }
+})
 function searchPicture(){
- $.get("http://api.pexels.com/v1/search?", {query: })
+    // 563492ad6f9170000100000172ef63d0155144725a94b9b7b795b842 
+    $.ajax({
+        url: "http://api.pexels.com/v1/search?query=" + document.getElementById('photoSearch').value + "&per_page=15&page=1",
+        headers: {
+            "Authorization": "563492ad6f9170000100000172ef63d0155144725a94b9b7b795b842"
+        },
+        success: (data) => infiniteScrollImages(data)
+    })
+}
+
+function infiniteScrollImages(images) {
+    for (pix in images.photos){
+        let pixNode = document.createElement('img').setAttribute("src", images.photos[pix].src.original)
+        document.getElementById('photosDisplay').appendChild(pixNode)
+    }
 }
