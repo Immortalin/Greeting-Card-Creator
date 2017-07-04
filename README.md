76769a9c56cb68aa97d1a022af41130e9ef7c3e6
Immortalin <immortalin@icloud.com>
2017-07-03 20:15:40 -0700

    TIS WORKING!!!

---
+++ README.md
@@ -0,0 +1,9 @@
+# Greeting Card Creator 
+
+This was/is designed for iogifts
+
+## Development
+git clone --recursive https://github.com/Immortalin/Greeting-Card-Creator
+
+If miniPaint submodule is edited, rebase upon fork
+
--- search.js
+++ search.js
@@ -17,15 +17,32 @@ function searchPicture(){
 
 function infiniteScrollImages(images) {
     for (pix in images.photos){
-        let pixNode = document.createElement('img')
-        pixNode.src = images.photos[pix].src.original
-        pixNode.className = "img-thumbnail"
+        // let pixNode = document.createElement('img')
+        let pixNode = document.createElement('div')
+        let pixNodeImg = document.createElement('img')
+        let pixNodeOverlay = document.createElement('div')
+        let pixNodeText = document.createElement('a')
+        // let pixNode = document.createElement('option')
+        // pixNode.setAttribute('data-img-src', images.photos[pix].src.small)
+        // pixNode.src = images.photos[pix].src.small
+        pixNodeImg.src = images.photos[pix].src.small
+        pixNodeImg.className = "card-img"
+        pixNode.className = "card"
+        pixNodeOverlay.className = "card-img-overlay"
+        // pixNode.className = "img-thumbnail"
+        pixNodeText.href = window.location.href + "miniPaint/index.html?input_image_url=" + images.photos[pix].src.original
+        pixNodeText.textContent = "Use this"
+        pixNodeText.className = "card-title"
+        pixNodeOverlay.appendChild(pixNodeText)
+        pixNode.appendChild(pixNodeImg)
+        pixNode.appendChild(pixNodeOverlay)
         document.getElementById('photosDisplay').appendChild(pixNode)
         console.log(pixNode)
         // https://masonry.desandro.com/#getting-started
-        const masonry = new Masonry('grid', {
-            itemSelector: '.grid-pix',
+        // const masonry = new Masonry('grid', {
+        //     itemSelector: '.grid-pix',
 
-        })
+        // })
     }
+    // $("select[name='image-selector']").imagepicker()
 }
